0eb128dc2112b046d429b866f790a3421f69cea0
Chelston Juntilla <chejun4790@gmail.com>
2020-01-12 14:50:00 -0700

    Various improvements
    * Better draggable collision detection
    * Added table background for coin minigame

--- Assets/Scripts/MovementAndControl/DraggableController.cs
+++ Assets/Scripts/MovementAndControl/DraggableController.cs
@@ -8,10 +8,10 @@ using UnityEngine.Events;
 public class DraggableController : MonoBehaviour
 {
 	public UnityEvent dropMethod, dragMethod;
+	public float speed = 99f;
 	
 	private Vector3 _screenPoint;
 	private Vector3 _offset;
-	private bool _isDragging;
 	private SpriteRenderer _renderer;
 	private Rigidbody2D _rb;
 
@@ -29,15 +29,18 @@ public class DraggableController : MonoBehaviour
 		_renderer.sortingOrder = 1;
 	}
 		
-	void OnMouseDrag(){
+	void OnMouseDrag()
+	{
 		Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z);
 		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + _offset;
-		_rb.MovePosition(new Vector2(cursorPosition.x, cursorPosition.y));
+		// _rb.MovePosition(new Vector2(cursorPosition.x, cursorPosition.y));
+		_rb.velocity = (new Vector2(cursorPosition.x, cursorPosition.y) - new Vector2(gameObject.transform.position.x, gameObject.transform.position.y)) * speed;
 	}
 
 	void OnMouseUp()
 	{
 		dropMethod.Invoke();
 		_renderer.sortingOrder = 0;
+		_rb.velocity = new Vector2(0, 0);
 	}
 }
