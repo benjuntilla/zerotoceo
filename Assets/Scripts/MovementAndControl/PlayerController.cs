850eeedeae7e026a6aed4d5529ab66e0762f9415
Chelston Juntilla <chejun4790@gmail.com>
2020-01-19 20:29:17 -0700

    Removed CharacterController2D

--- Assets/Scripts/MovementAndControl/PlayerController.cs
+++ Assets/Scripts/MovementAndControl/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using Prime31;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.WebSockets;
@@ -11,21 +11,16 @@ public class PlayerController : MonoBehaviour
 	public static int Points;
 	public static int Lives = 3;
 	[Header("Movement config")]
-    public float gravity = -10f;
-    public float runSpeed = 2f;
-    public float groundDamping = 99f; // how fast do we change direction? higher means faster
-    public float inAirDamping = 99f;
-    public float jumpHeight = 1f;
-    public float fallThreshold = -5f;
-    public float jumpThreshold = 1f;
-    public Vector3 velocity;
-    
+	public float runSpeed = 1f;
+    public float jumpHeight = 10f;
+    public Vector2 velocity;
+
     private InteractableController[] _interactables;
     private Dictionary<InteractableController, float> _interactablesDistances = new Dictionary<InteractableController, float>();
     private GameObject _indicatorUI;
-    private float _normalizedHorizontalSpeed;
-    private CharacterMovement2D _characterMovement;
     private Animator _animator;
+    private Rigidbody2D _rb;
+    private bool _isGrounded;
 
     void Awake()
     {
@@ -33,7 +28,7 @@ public class PlayerController : MonoBehaviour
         _indicatorUI = GameObject.Find("Indicator");
         
         _animator = GetComponent<Animator>();
-        _characterMovement = GetComponent<CharacterMovement2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     private void UpdateIndicator()
@@ -62,87 +57,72 @@ public class PlayerController : MonoBehaviour
 	    }
     }
 
+    void OnCollisionStay2D()
+    {
+	    _isGrounded = true;
+    }
+
     void Update()
     {
 	    UpdateIndicator();
-        
-        #region player movement
-        
-	    if( _characterMovement.isGrounded )
-			velocity.y = 0;
+	    
+	    if (!godMode) return;
+	    Points = 9999;
+	    Lives = 9999;
+    }
 
-		if( Input.GetAxisRaw("Horizontal") == 1 )
+    void FixedUpdate()
+    {
+	    #region player movement
+        if( Input.GetAxisRaw("Horizontal") == 1 )
 		{
-			_normalizedHorizontalSpeed = 1;
+			velocity.x = 1;
+			// Modify appearance
 			if( transform.localScale.x < 0f && Time.timeScale == 1f)
 				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
-
-			if( _characterMovement.isGrounded )
+			if( _isGrounded )
 				_animator.Play( Animator.StringToHash( "Run" ) );
 		}
 		else if( Input.GetAxisRaw("Horizontal") == -1 )
 		{
-			_normalizedHorizontalSpeed = -1;
+			velocity.x = -1;
+			// Modify appearance
 			if( transform.localScale.x > 0f && Time.timeScale == 1f)
 				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
-
-			if( _characterMovement.isGrounded )
+			if( _isGrounded )
 				_animator.Play( Animator.StringToHash( "Run" ) );
 		}
 		else
 		{
-			_normalizedHorizontalSpeed = 0;
-
-			if( _characterMovement.isGrounded )
+			velocity.x = 0;
+			// Modify appearance
+			if( _isGrounded )
 				_animator.Play( Animator.StringToHash( "Idle" ) );
 		}
 
 		// we can only jump whilst grounded
-		if( _characterMovement.isGrounded && Input.GetAxisRaw("Vertical") == 1 )
+		if( (_isGrounded && Input.GetAxisRaw("Vertical") == 1) || (_isGrounded && Input.GetButtonDown("Jump")) )
 		{
-			velocity.y = Mathf.Sqrt( 2f * jumpHeight * -gravity );
+			_isGrounded = false;
+			_rb.AddForce(new Vector3(0.0f, 2.0f, 0.0f) * jumpHeight, ForceMode2D.Impulse);		
 		}
-		if( _characterMovement.isGrounded && Input.GetButtonDown("Jump") )
+		else
 		{
-			velocity.y = Mathf.Sqrt( 2f * jumpHeight * -gravity );
-		}
-
-		// play falling animation when visibly moving downwards
-		if( !_characterMovement.isGrounded && velocity.y < fallThreshold)
-		{
-			_animator.Play( Animator.StringToHash( "Fall" ) );
+			velocity.y = 0;
 		}
 
 		// play jumping animation when visibly moving upwards
-		if( !_characterMovement.isGrounded && velocity.y > jumpThreshold)
+		if( !_isGrounded )
 		{
 			_animator.Play( Animator.StringToHash( "Jump" ) );
 		}
 
-		// apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
-		var smoothedMovementFactor = _characterMovement.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
-		velocity.x = Mathf.Lerp( velocity.x, _normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor );
-
-		// apply gravity before moving
-		velocity.y += gravity * Time.deltaTime;
-
-		// if holding down bump up our movement amount and turn off one way platform detection for a frame.
-		// this lets us jump down through one way platforms
-		if( _characterMovement.isGrounded && Input.GetAxisRaw("Vertical") == -1 )
-		{
-			velocity.y *= 3f;
-			_characterMovement.ignoreOneWayPlatformsThisFrame = true;
-		}
-
-		_characterMovement.move( velocity * Time.deltaTime );
-
-		// grab our current _velocity to use as a base for all calculations
-		velocity = _characterMovement.velocity;
+		velocity.x *= runSpeed;
 		
+		// transform.Translate(velocity * Time.deltaTime * runSpeed);
+		_rb.AddForce( velocity );
+		// _rb.velocity = velocity;
+		// _rb.MovePosition( velocity );
 		#endregion
-		
-		if (!godMode) return;
-		Points = 9999;
-		Lives = 9999;
     }
 }
